f6edf4ead01111eba5ab09e50b88187bc8e35f2a
DESKTOP-EBSFH8K\egorj <egor.jorik@yandex.ru>
2021-07-18 17:36:02 +0300

    initial commit

---
+++ jsonEvent/jsonEvent/Services/IUserDB.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace jsonEvent.Services
+{
+    /// <summary>
+    /// бд с пользователями
+    /// </summary>
+    public interface IUserDB
+    {
+        void AddUser(int id);
+        /// <summary>
+        /// проверка: есть ли такой id в бд
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        bool TryUser(int id);
+        int UsersCount();
+    }
+}
---
+++ jsonEvent/jsonEvent/Services/UserAuthService.cs
@@ -0,0 +1,50 @@
+﻿
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace jsonEvent.Services
+{
+    public class UserAuthService : IUserAuthService
+    {
+        private IUserDB _userDB;
+        private int _userId;
+        public UserAuthService(IUserDB userDB)
+        {
+            _userDB = userDB;
+        }
+        private void AddNewUser(HttpContext context)
+        {
+            // не оч хороший способ...
+            int newUserId = _userDB.UsersCount();
+            while (true)
+            {
+                if (!_userDB.TryUser(newUserId))
+                    break;
+            }
+            context.Response.Cookies.Append("UserId", newUserId.ToString());
+            _userDB.AddUser(newUserId);
+            _userId = newUserId;
+        }
+        public void Auth(HttpContext context)
+        {
+            //
+            var value = context.Request.Cookies["UserId"];
+            int uid = Int32.Parse(value);
+            if (value == null || _userDB.TryUser(uid))
+            {
+                AddNewUser(context);
+            }
+            else
+            {
+                _userId = uid;
+            }
+        }
+        public int UserId
+        {
+            get { return _userId; }
+        }
+    }
+}
